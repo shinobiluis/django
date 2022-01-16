266af5740041bad9585b072579baa14346b99dd8
Luis Eduardo Alcantara Olvera <shinobi_luis@hotmail.es>
2022-01-16 08:28:35 -0600

    Registramos el modelo al admin de django

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from posts.models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'profile', 'title', 'photo')
+
+    # Hacer que al dar click al numero nos abra el registro
+    list_display_links = ('pk' ,'user', 'title')
