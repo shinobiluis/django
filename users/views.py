dc89afb5324a650454a714c2b8b511aae27b7774
Luis Eduardo Alcantara Olvera <shinobi_luis@hotmail.es>
2022-01-17 23:08:06 -0600

    Se creo el registro de usuarios

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from users.models import Profile
+from django.db.utils import IntegrityError
 
 # Create your views here.
 def login_view( request ):
@@ -22,3 +25,29 @@ def login_view( request ):
 def logout_view( request ):
     logout(request)
     return redirect('login');
+
+
+def signup( request ):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+        if password != password_confirmation:
+            return render(request, 'users/signup.html', {'error':'El password debe ser igual'})
+        
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error':'El usaurio ya existe'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
