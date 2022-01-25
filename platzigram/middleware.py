bc98299f03ea155ebc1f4797c1b363eea073a86d
Luis Eduardo Alcantara Olvera <shinobi_luis@hotmail.es>
2022-01-24 22:09:08 -0600

    Se crea middleware de ejemplo

--- platzigram/middleware.py
+++ platzigram/middleware.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
 class ProfileCompletionMiddleware:
     def __init__(self, get_response):
@@ -8,7 +9,8 @@ class ProfileCompletionMiddleware:
         if not request.user.is_anonymous:
             profile = request.user.profile
             if not profile.picture or not profile.biography:
-                return redirect('update_profile')
+                if request.path not in [reverse('update_profile'), reverse('logout')]:
+                    return redirect('update_profile')
         
         response = self.get_response(request)
         return response
--- platzigram/settings.py
+++ platzigram/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'platzigram.middleware.ProfileCompletionMiddleware',
 ]
 
 ROOT_URLCONF = 'platzigram.urls'
@@ -69,7 +70,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'platzigram.middleware.ProfileCompletionMiddleware',
             ],
         },
     },
